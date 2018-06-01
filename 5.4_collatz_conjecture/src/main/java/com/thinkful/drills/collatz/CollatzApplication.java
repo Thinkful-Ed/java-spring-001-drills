38d1551060b4f71705cce2a1fc28ed46e4b6dca1
Vincent Ramdhanie <vramdhanie@gmail.com>
2018-06-01 09:53:33 -0400

    Format generated code

--- 5.4_collatz_conjecture/src/main/java/com/thinkful/drills/collatz/CollatzApplication.java
+++ 5.4_collatz_conjecture/src/main/java/com/thinkful/drills/collatz/CollatzApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CollatzApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CollatzApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(CollatzApplication.class, args);
+  }
 }
