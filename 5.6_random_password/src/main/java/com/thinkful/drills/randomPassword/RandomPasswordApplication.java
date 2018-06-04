e9d65189f6c1db917d250143991bcb1bb56a1c1d
Vincent Ramdhanie <vramdhanie@gmail.com>
2018-06-04 11:39:55 -0400

    Implement the password generator

--- 5.6_random_password/src/main/java/com/thinkful/drills/randomPassword/RandomPasswordApplication.java
+++ 5.6_random_password/src/main/java/com/thinkful/drills/randomPassword/RandomPasswordApplication.java
@@ -1,4 +1,4 @@
-package com.thinkful.drills.randomPassword;
+package com.thinkful.drills.randompassword;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
