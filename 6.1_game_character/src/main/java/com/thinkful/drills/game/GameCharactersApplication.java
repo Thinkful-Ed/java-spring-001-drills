03e2e830a1dac7948f7bfd2d7703b078e0516c48
Vincent Ramdhanie <vramdhanie@gmail.com>
2018-06-05 10:15:22 -0400

    Initialize new Spring Boot application

---
+++ 6.1_game_character/src/main/java/com/thinkful/drills/game/GameCharactersApplication.java
@@ -0,0 +1,12 @@
+package com.thinkful.drills.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameCharactersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameCharactersApplication.class, args);
+	}
+}
