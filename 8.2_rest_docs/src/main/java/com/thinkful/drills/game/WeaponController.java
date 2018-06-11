ff27f6954a559e40f8fa7cf16950af0198b7abf6
Vincent Ramdhanie <vramdhanie@gmail.com>
2018-06-10 21:20:55 -0400

    Configure global and controller specific CORS

--- 8.2_rest_docs/src/main/java/com/thinkful/drills/game/WeaponController.java
+++ 8.2_rest_docs/src/main/java/com/thinkful/drills/game/WeaponController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("/api/weapons")
 public class WeaponController {
